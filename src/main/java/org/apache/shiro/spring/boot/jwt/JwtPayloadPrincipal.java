3f7a5640b1d9a38aded6078ab0a1e8d04ff7b1db
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 22:22:52 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

--- src/main/java/org/apache/shiro/spring/boot/jwt/JwtPayloadPrincipal.java
+++ src/main/java/org/apache/shiro/spring/boot/jwt/JwtPayloadPrincipal.java
@@ -17,7 +17,7 @@ package org.apache.shiro.spring.boot.jwt;
 
 import org.apache.shiro.biz.authz.principal.ShiroPrincipal;
 
-import io.github.hiwepy.jwt.JwtPayload;
+import io.github.easy4j.jwt.JwtPayload;
 
 /**
  * TODO
